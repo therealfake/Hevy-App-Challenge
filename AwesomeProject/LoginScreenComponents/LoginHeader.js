ddb78e90b7d4ab928c0eb918474fad159ca0ba90
Tianji Zhang <zhang.sh.tianji@gmail.com>
2024-04-21 15:18:28 -0400

    login page header section'

---
+++ AwesomeProject/LoginScreenComponents/LoginHeader.js
@@ -0,0 +1,39 @@
+import { 
+    StyleSheet,
+    Text, 
+    Button,
+    View } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "row",
+        alignSelf: "stretch",
+        paddingTop: 30,
+        gap:120
+    },
+    backButton: {
+    },
+    headerText: {
+        color: "#fff",
+        flex: 1,
+        alignSelf: "center",
+    }
+    });
+    
+const LoginHeader = () => {
+    return (
+        <View style={styles.container}>
+            <Icon.Button
+                name="arrowleft"
+                color="#1C83EA"
+                backgroundColor="transparent"
+                style={styles.backButton}
+            >
+            </Icon.Button>
+            <Text style={styles.headerText}> Login </Text>
+      </View>
+    );
+}
+export default LoginHeader;
